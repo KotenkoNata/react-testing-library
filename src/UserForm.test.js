dd9487f09282c77db32c002c87f5b4a9153fceba
KotenkoNata <kotenkonata8@gmail.com>
2024-05-22 15:00:34 +0200

    feat: Simulating User Events

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -14,3 +14,17 @@ test('it shows two inputs and a button', ()=>{
     expect(button).toBeInTheDocument();
 
 });
+
+test('it calls onUserAdd when the form is submitted', ()=>{
+    render(<UserForm />);
+
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+    user.click(nameInput);
+    user.keyboard('jane');
+
+    user.click(emailInput);
+    user.keyboard('jane@gmail.com');
+
+
+})
