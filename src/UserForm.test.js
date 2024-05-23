d25326d39a317ac4e7060da551c518bb61e2cea0
KotenkoNata <kotenkonata8@gmail.com>
2024-05-23 15:27:36 +0200

    feat: Recording Function Calls

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -16,7 +16,13 @@ test('it shows two inputs and a button', ()=>{
 });
 
 test('it calls onUserAdd when the form is submitted', ()=>{
-    render(<UserForm />);
+    const argList = [];
+
+    const callback = (...args)=>{
+        argList.push(args)
+    }
+
+    render(<UserForm onUserAdd={callback}/>);
 
     const [nameInput, emailInput] = screen.getAllByRole('textbox');
 
@@ -26,5 +32,9 @@ test('it calls onUserAdd when the form is submitted', ()=>{
     user.click(emailInput);
     user.keyboard('jane@gmail.com');
 
+    const button = screen.getByRole('button');
+    user.click(button);
 
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({name: 'jane', email: 'jane@gmail.com'});
 })
