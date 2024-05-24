f5e9254fed54addf65d1e4b9904a3721366fe020
KotenkoNata <kotenkonata8@gmail.com>
2024-05-24 14:11:20 +0200

    feat: A Touch of Test Driven Development

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -40,3 +40,22 @@ test('it calls onUserAdd when the form is submitted', ()=>{
     expect(mock).toHaveBeenCalled();
     expect(mock).toHaveBeenCalledWith({name: 'jane', email: 'jane@gmail.com'});
 })
+
+test('empties the two inputs when form is submitted', ()=>{
+    render(<UserForm onUserAdd={()=>{}}/>);
+
+    const nameInput = screen.getByRole('textbox', {name: /name/i});
+    const emailInput = screen.getByRole('textbox', {name: /email/i});
+
+    user.click(nameInput);
+    user.keyboard('jane');
+    user.click(emailInput);
+    user.keyboard('jane@gmail.com');
+
+    const button = screen.getByRole('button');
+
+    user.click(button);
+
+    expect(nameInput).toHaveValue('');
+    expect(emailInput).toHaveValue('');
+})
