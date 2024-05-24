1fb11141e5f6bc273ae6f2107ab98162e4f6f30c
KotenkoNata <kotenkonata8@gmail.com>
2024-05-24 13:29:22 +0200

    feat: Avoiding BeforeEach

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,25 +1,31 @@
 import {render, screen, within} from "@testing-library/react";
 import UserList from "./UserList";
 
-test('render one row per user', ()=>{
+function renderComponent() {
     const users = [
         {name: "John", email: "john@example.com"},
         {name: "Jane", email: "Jane@example.com"},
     ]
     render(<UserList users={users} />);
+
+    return {
+        users,
+    }
+}
+
+test('render one row per user', ()=>{
+    renderComponent();
 
     const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
     expect(rows).toHaveLength(2)
 })
 
+
+
 test('render the email and name of each user', ()=>{
 
-    const users = [
-        {name: "John", email: "john@example.com"},
-        {name: "Jane", email: "Jane@example.com"},
-    ]
-    render(<UserList users={users} />);
+    const {users} = renderComponent();
 
     for(let user of users){
         const name = screen.getByRole('cell', {name: user.name});
